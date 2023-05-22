72d4c1990152216b87c91c5aa480b9ebfc0b2a1c
sadorect <sadorect@gmail.com>
2023-05-22 04:31:48 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,46 +16,7 @@ class nginx_install {
                   server_name _;
                   root /var/www/html;
                   location / {
-                    return 301 http://class nginx_install {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  file { '/var/www/html/index.html':
-    ensure  => present,
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => "server {
-                  listen 80;
-                  server_name _;
-                  root /var/www/html;
-                  location / {
-                    return 301 http://https://www.youtube.com/;
-                  }
-                }",
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  file { '/etc/nginx/sites-enabled/default':
-    ensure => 'link',
-    target => '/etc/nginx/sites-available/default',
-    require => File['/etc/nginx/sites-available/default'],
-  }
-
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => [Package['nginx'], File['/etc/nginx/sites-enabled/default']],
-  }
-}
-
-include nginx_install
-;
+                    return 301 https://www.youtube.com;
                   }
                 }",
     require => Package['nginx'],
