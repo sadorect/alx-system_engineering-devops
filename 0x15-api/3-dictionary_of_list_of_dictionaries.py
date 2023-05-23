d6ac741fd59c43741f2d26c7a7d57b17cd8fa72b
sadorect <sadorect@gmail.com>
2023-05-23 15:26:48 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
+"""
+TODO Export Script
+
+This script retrieves TODO list data for all employees from a REST API and exports it in JSON format.
+
+Usage: python3 todo_export_all_employees.py
+"""
+
 import requests
 import json
 
 def export_todo_all_employees():
+    """
+    Export TODO list data for all employees in JSON format.
+    """
+
     base_url = "https://jsonplaceholder.typicode.com"
     users_url = f"{base_url}/users"
     todos_url = f"{base_url}/todos"
@@ -19,14 +31,14 @@ def export_todo_all_employees():
     # Prepare TODO list data in JSON format
     json_data = {}
     for user in users_data:
-        user_id = user["id"]
-        username = user["name"]
-        user_todos = [task for task in todos_data if task["userId"] == user_id]
+        user_id = user.get("id")
+        username = user.get("name")
+        user_todos = [task for task in todos_data if task.get("userId") == user_id]
         json_data[user_id] = [
             {
                 "username": username,
-                "task": task["title"],
-                "completed": task["completed"]
+                "task": task.get("title"),
+                "completed": task.get("completed")
             }
             for task in user_todos
         ]
@@ -37,6 +49,7 @@ def export_todo_all_employees():
         json.dump(json_data, file)
 
     print(f"TODO list data for all employees exported to {filename}.")
+
 
 if __name__ == "__main__":
     export_todo_all_employees()
