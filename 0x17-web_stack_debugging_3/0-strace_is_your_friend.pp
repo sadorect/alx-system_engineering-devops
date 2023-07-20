fb860e73d6697beaaf5e9b9cca1d64ea4ad18785
OluwaTosin Omojola <sadorect@gmail.com>
2023-07-20 23:57:42 +0100

    deferment to new cohort

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# puppet manifest to find out why Apache is returning a 500 error.
-
-# replace bad file extension phpp with php:
-exec { 'sed_repl_phpp':
-  command => '/bin/sed -i --follow-symlinks "s/phpp/php/g" \
-    /var/www/html/wp-settings.php',
-}
